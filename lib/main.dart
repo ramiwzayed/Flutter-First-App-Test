d7b3816a44891befe4cd06441a7936e24025b886
Rami Zayed <ramiwzayed@gmail.com>
2024-09-22 00:08:17 +0300

    Added background color

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hellow World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+            ),
+          ),
+          // backgroundColor: Color.fromRGBO(255, 255, 0, 1),
+          child: const Center(
+            child: Text('Hellow World!'),
+          ),
         ),
       ),
     ),
