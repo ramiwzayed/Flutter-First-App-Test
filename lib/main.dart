053a0cb9d90fb59df15a33f4d9eb96daf1df7ed8
Rami Zayed <ramiwzayed@gmail.com>
2024-10-07 22:01:44 +0300

    Building Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -30,3 +30,21 @@ void main() {
     ),
   );
 }
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+    );
+  }
+}
