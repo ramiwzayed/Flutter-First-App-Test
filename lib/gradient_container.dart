6aa11f1efccd9e588a5cd2b494697985967cd160
Rami Zayed <ramiwzayed@gmail.com>
2024-10-11 22:36:15 +0300

    Added Var

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:first_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget{
   const GradientContainer{(super.key)};
