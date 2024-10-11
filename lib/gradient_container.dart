9ad39e7db64b689c2f426da076a895bd12c39a4d
Rami Zayed <ramiwzayed@gmail.com>
2024-10-11 22:28:36 +0300

    Create custom Widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
-import 'package:basics/gradient_container.dart';
-
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(),
+class GradientContainer extends StatelessWidget{
+  const GradientContainer{(super.key)};
+  
+  @override
+  Widget build(context) {
+    // TODO: implement build
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255 , 26 , 2,80),
+            Color.fromARGB(255, 45, 7, 98),
+          ], 
+          begin: Alignment.topLeft,
+          end:Alignment.bottomRight,
+        )
       ),
-    ),
-  );
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+            color:Color.fromARGB(0, 255, 255, 255),
+            fontSize: 28,
+          ),
+        )
+      ),
+    )
+  }
 }
